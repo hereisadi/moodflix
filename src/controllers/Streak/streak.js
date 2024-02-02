0a362a6e28da97f54a25702a3ea31210ce5cf71c
hereisadi <hereisadi@proton.me>
2024-02-03 01:58:44 +0530

    ADD:fetch streak

---
+++ src/controllers/Streak/streak.js
@@ -0,0 +1,60 @@
+const { verifyToken } = require("../../middlewares/VerifyToken");
+const { SearchModel } = require("../../models/Search");
+const { SignUpModel } = require("../../models/User");
+// const moment = require("moment");
+
+const getStreaks = async (req, res) => {
+  verifyToken(req, res, async () => {
+    try {
+      const userId = req.user.userId;
+      if (!userId) {
+        return res.status(401).json({ error: "Unauthorized" });
+      }
+
+      const user = await SignUpModel.findById(userId);
+
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+
+      const particularSearch = await SearchModel.findOne({ email: user.email });
+      const array = particularSearch?.searchs;
+
+      // sort the array in descending order
+
+      if (!array || array.length === 0) {
+        return res.status(200).json({ streak: 0 });
+      }
+
+      let streak = 0;
+      let largestDay;
+
+      for (let i = array.length - 1; i >= 0; i--) {
+        if (i === array.length - 1) {
+          const date = array[i].time;
+          const extractedDay = date.split(" ")[0].split("-")[0];
+          largestDay = extractedDay;
+        } else {
+          const currentDatesDay = array[i].time.split(" ")[0].split("-")[0];
+          console.log(currentDatesDay);
+
+          if (largestDay - currentDatesDay === 1) {
+            streak++;
+            largestDay = currentDatesDay;
+          } else {
+            continue;
+          }
+        }
+      }
+
+      return res.status(200).json({ streak });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "server error" });
+    }
+  });
+};
+
+module.exports = {
+  getStreaks,
+};
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -6,15 +6,18 @@ const { dashboard } = require("../controllers/Auth/Dashboard");
 const { searchsMade } = require("../controllers/Searchs/Search");
 const { sentimentAnalysis } = require("../controllers/Searchs/Sentiment");
 const { botResponse } = require("../controllers/Bot/BotResult");
+const { getStreaks } = require("../controllers/Streak/streak");
 
 const router = express.Router();
 
 router.get("/", home);
 router.get("/me", dashboard);
 router.get("/sentiment", sentimentAnalysis);
+router.get("/streak", getStreaks);
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/search", searchsMade);
-router.post("/bot", botResponse);
+router.get("/bot", botResponse);
+
 module.exports = router;
