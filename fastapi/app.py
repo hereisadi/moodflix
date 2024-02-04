44b98e1f9f33212863518281d2ad3f258e9901d0
prateek <Prateek.mogha6@gmail.com>
2024-02-04 05:44:43 +0530

    added the model

--- fastapi/app.py
+++ fastapi/app.py
@@ -3,9 +3,18 @@ from pydantic import BaseModel
 # import data
 import pickle
 import json
-import uvicorn
+# import uvicorn
 import numpy as np
 from starlette.middleware.cors import CORSMiddleware
+# import streamlit as st
+import numpy as np
+import re
+from nltk.stem import PorterStemmer
+import pickle
+import nltk
+from tensorflow.keras.models import load_model
+
+
 
 appp = FastAPI()
 
@@ -25,25 +34,60 @@ class model_input(BaseModel):
     feeling: str
 
 
-# loading the saved model
-# just need to input ml model file here
-feeling_model = pickle.load(open('model.h5', 'rb'))
 
 
-@appp.post('/feeling_prediction')
-def feeling_pred(input_parameters: model_input):
+nltk.download('stopwords')
+stopwords = set(nltk.corpus.stopwords.words('english'))
 
-    # input_data = input_parameters.json()
-    # input_dictionary = json.loads(input_data)
-    input_data = input_data.text()
+
+lg = pickle.load(open('/home/wildfire/moodflix/fastapi/MODEL.pkl','rb'))
+tfidf_vectorizer = pickle.load(open('/home/wildfire/moodflix/fastapi/vectorizer.pkl','rb'))
+lb = pickle.load(open('/home/wildfire/moodflix/fastapi/label_encoder.pkl','rb'))
+
+
+def clean_text(text):
+    stemmer = PorterStemmer()
+    text = re.sub("[^a-zA-Z]", " ", text)
+    text = text.lower()
+    text = text.split()
+    text = [stemmer.stem(word) for word in text if word not in stopwords]
+    return " ".join(text)
+
+def predict_emotion(input_text):
+    cleaned_text = clean_text(input_text)
+    input_vectorized = tfidf_vectorizer.transform([cleaned_text])
+
+    # Predict emotion
+    predicted_label = lg.predict(input_vectorized)[0]
+    predicted_emotion = lb.inverse_transform([predicted_label])[0]
+    label =  np.max(lg.predict(input_vectorized))
+
+    return predicted_emotion,label
+
+sentence = 'I love her so much'
+
+emotion , what = predict_emotion(sentence)
+print(f"{emotion} : {what}\n\n")
+
+
+
+
+# @appp.post('/feeling_prediction')
+
+
+
+# def feeling_pred(input_parameters: model_input):
+
+#     # input_data = input_parameters.json()
+#     # input_dictionary = json.loads(input_data)
+#     input_data = input_data.text()
     
-    print(input_data)
+#     print(input_data)
 
-    prediction = feeling_model.predict(np.array([input_data]))
+#     prediction = feeling_model.predict(np.array([input_data]))
 
-    return {
-        prediction
-    }
-
-if __name__ == '__main__':
-    uvicorn.run(appp, host='127.0.0.1', port=8000)
+#     return {
+#         prediction
+#     }
+# if __name__ == '__main__':
+#     uvicorn.run(appp, host='127.0.0.1', port=8000)
